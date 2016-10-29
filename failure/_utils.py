6816118abc03713d5358865b2b33082d62c172e7
Joshua Harlow <jxharlow@godaddy.com>
2016-10-29 18:44:30 +0200

    Initial shift from taskflow to this library

---
+++ failure/_utils.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright (C) 2014 Yahoo! Inc. All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import copy
+import traceback
+
+from oslo_utils import encodeutils
+
+
+exception_message = encodeutils.exception_to_unicode
+
+
+def copy_exc_info(exc_info):
+    if exc_info is None:
+        return None
+    exc_type, exc_value, tb = exc_info
+    # NOTE(imelnikov): there is no need to copy the exception type, and
+    # a shallow copy of the value is fine and we can't copy the traceback since
+    # it contains reference to the internal stack frames...
+    return (exc_type, copy.copy(exc_value), tb)
+
+
+def are_equal_exc_info_tuples(ei1, ei2):
+    if ei1 == ei2:
+        return True
+    if ei1 is None or ei2 is None:
+        return False  # if both are None, we returned True above
+    # NOTE(imelnikov): we can't compare exceptions with '=='
+    # because we want exc_info be equal to it's copy made with
+    # copy_exc_info above.
+    if ei1[0] is not ei2[0]:
+        return False
+    if not all((type(ei1[1]) == type(ei2[1]),  # noqa
+                exception_message(ei1[1]) == exception_message(ei2[1]),
+                repr(ei1[1]) == repr(ei2[1]))):
+        return False
+    if ei1[2] == ei2[2]:
+        return True
+    tb1 = traceback.format_tb(ei1[2])
+    tb2 = traceback.format_tb(ei2[2])
+    return tb1 == tb2
